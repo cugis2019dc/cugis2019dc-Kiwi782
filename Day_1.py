0b3fd7c014350bedd26e1d7684b83c039a83d391
Kiwi782 <53440819+Kiwi782@users.noreply.github.com>
2019-07-31 10:21:54 -0400

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print (8)
+print(5*2)
+print(5/2)
+print(5**2)
+print(8/9*3)
+5-2
+5*2
+8/9*3
+
+def multiply (a,b):
+    multiply = a*b 
+    
+    print(multiply)
+    
+multiply(4,5)
+
+
+def add(a,b,c):
+    add = a+b+c
+    
+    print(add)
+    
+
+add(9,8,9)
+
+def triangle(base,height):
+    triangle = 1/2 * base * height
+   
+    print("The area of the triangle with", base ,"and", height ,"is", triangle)
+    
+triangle(12,5)
+
+def caburybox(a,b):
+    caburybox=a+b
+    
+    print(caburybox)
+  
+caburybox(15,5)
+
+a = "Milk Chocolate"
+b = "Dark Chocolate"
+c = "White Chocolate"
+    
+print(a,b,c)
+  
+print ("Hello Kaya")  
+
+a = "Kaya"
+print("Hello",a)
+
+name = input("please enter your name")
+print("Your name is", name)
+
+age = input("please enter your age")
+print("Thanks.", "You enetered", age)
+
+ageint = int(age)
+ageint
+
+ageint = float(age)
+
+import math
+
+dir (math)
+
+math.pi
+
+math.pow(6,15)
+
+def cuberoot(a):
+    print("The cubic root of",a, "is", math.pow(a,(1/3)))
+    
+    cuberoot(16)
+    
+    import math
+    a=int(input("Enter a value"))
+    
+    def cadbury (a):
+        cadbury= math.pow(a,1/3)
+        print("The cubic root of", a, "is", cadbury)
+        
+        cadbury(a)
+    
+    
+    
+    
+    
+    
+    
+    
